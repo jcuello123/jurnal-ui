8f950b52480e0d8232a7c9959d3e485e57165b64
jcuello123 <jcuello123@yahoo.com>
2022-07-18 20:24:59 -0400

    navigate to initial 3 logs using arrows

--- src/component/LogView.tsx
+++ src/component/LogView.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiService } from "../service/api.service";
 import { sessionService } from "../service/session.service";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import moment from "moment";
 
 interface Log {
 	text: string;
@@ -14,9 +16,10 @@ const LogView = () => {
 	const [logs, setLogs] = useState<Log[]>([]);
 	const emptyLog: Log = {
 		text: "",
-		date: new Date().toLocaleDateString("en-US"),
+		date: moment().format("YYYY-MM-DD"),
 	};
 	const [currentLog, setCurrentLog] = useState<Log>(emptyLog);
+	const [index, setCurrentIndex] = useState(0);
 
 	useEffect(() => {
 		const token = sessionService.getToken();
@@ -28,32 +31,69 @@ const LogView = () => {
 		}
 
 		const fetchLogs = async () => {
-			const data: Log[] = await apiService.getLogs(offset, fetchLogs);
+			const data: Log[] = (await apiService.getLogs(offset, fetchLogs)) || [];
+			console.log(data);
 
-			if (data && data.length > 0) {
-				setLogs(data);
-				setCurrentLog(data[0]);
+			if (data.length === 0 || data[0].date !== emptyLog.date) {
+				data.unshift(emptyLog);
 			}
+
+			setLogs(data);
+			setCurrentLog(data[0]);
 		};
 
 		fetchLogs();
 	}, []);
 
-	const formatDate = (date) => {
-		return new Date(date).toLocaleDateString("en-US");
+	useEffect(() => {
+		setCurrentLog(logs[index]);
+	}, [index]);
+
+	const handlePreviousLog = () => {
+		if (index < logs.length - 1) {
+			setCurrentIndex(index + 1);
+		}
+	};
+
+	const handleNextLog = () => {
+		if (index >= 1) {
+			setCurrentIndex(index - 1);
+		}
 	};
 
 	return (
-		<div
-			className="flex flex-col justify-center items-center absolute 
-					   top-[10%] left-[50%] -translate-x-1/2 text-white"
-		>
-			<div className="flex flex-col items-center">
-				<p className="text-6xl">{formatDate(currentLog.date)}</p>
-				<textarea
-					className="text-center w-[800px] h-[400px] my-20 text-xl bg-[#282c34] resize-none"
-					defaultValue={currentLog.text}
-				></textarea>
+		<div className="flex absolute top-[10%] left-[50%] -translate-x-1/2">
+			<div>
+				<button onClick={handlePreviousLog} className="text-4xl">
+					{<FaAngleLeft />}
+				</button>
+			</div>
+			<div className="flex flex-col justify-center items-center text-white">
+				<div className="flex flex-col items-center">
+					<p className="text-6xl">
+						{currentLog ? currentLog.date : emptyLog.date}
+					</p>
+					{currentLog && currentLog.date === moment().format("YYYY-MM-DD") && (
+						<textarea
+							className="text-center w-[800px] h-[400px] my-20 text-xl bg-[#282c34] resize-none outline-none"
+							defaultValue={currentLog ? currentLog.text : emptyLog.text}
+							onChange={() => {}}
+							autoFocus
+							maxLength={1000}
+							placeholder="Enter a log for today"
+						></textarea>
+					)}
+					{currentLog && currentLog.date !== moment().format("YYYY-MM-DD") && (
+						<div className="text-center w-[800px] h-[400px] my-20 text-xl">
+							<p>{currentLog.text ? currentLog.text : ""}</p>
+						</div>
+					)}
+				</div>
+			</div>
+			<div>
+				<button onClick={handleNextLog} className="text-4xl">
+					{<FaAngleRight />}
+				</button>
 			</div>
 		</div>
 	);
